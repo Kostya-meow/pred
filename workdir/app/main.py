3e111386bffef7a3609d245daa89fddef9a31748
Kostya-meow <79147829+Kostya-meow@users.noreply.github.com>
2024-02-04 10:40:27 +0700

    main.py

--- workdir/app/main.py
+++ workdir/app/main.py
@@ -1,60 +1,61 @@
-from fastapi import FastAPI, Body, Depends, HTTPException, status, File
-from fastapi.security import OAuth2PasswordBearer
-
-api_keys = [
-    "akljnv13bvi2vfo0b0bw"
-]  # This is encrypted in the database
-
-oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")  # use token authentication
-
-
-def api_key_auth(api_key: str = Depends(oauth2_scheme)):
-    if api_key not in api_keys:
-        raise HTTPException(
-            status_code=status.HTTP_401_UNAUTHORIZED,
-            detail="Forbidden"
-        )
-
+from fastapi import FastAPI, UploadFile, File, Form, Depends, HTTPException, status
+from fastapi.templating import Jinja2Templates
+from fastapi.requests import Request
+from fastai.vision.all import *
+import shutil
+import os
 
 app = FastAPI()
+templates = Jinja2Templates(directory="templates")
 
+API_KEY = "your_api_key1"
 
-@app.get("/test", dependencies=[Depends(api_key_auth)])
-def add_post() -> dict:
-    return {
-        "data": "You used a valid API key"
-    }
+def get_api_key(api_key: str = Form(...)):
+    if api_key == API_KEY:
+        return True
+    else:
+        raise HTTPException(status_code=403, detail="Invalid API Key")
 
+@app.get('/')
+async def main(request: Request):
+    return templates.TemplateResponse('index.html', {'request': request, 'result': None})
 
-@app.post('/predict', dependencies=[Depends(api_key_auth)])
-
-async def root(file: bytes = File()):
-
+@app.post('/classify')
+async def classify(request: Request, file: UploadFile = File(...), api_key: str = Form(...), authorized: bool = Depends(get_api_key)):
+    if not file:
+        raise HTTPException(status_code=400, detail="File not found")
+    temp_file = f"tempfile.{file.filename.split('.')[-1]}"
+    with open(temp_file, "wb") as buffer:
+        shutil.copyfileobj(file.file, buffer)
+    
     learner = load_learner("app/model.pkl")
-
-    result = list(learner.predict(file))
+    result = learner.predict(temp_file)
+    
     name_pred = str(result[1])
     name_pred = int(name_pred[name_pred.find('(')+1:name_pred.find(')')])
     perc_pred = str(result[2][name_pred])
     perc_pred = round(float(perc_pred[perc_pred.find('(')+1:perc_pred.find(')')]),2)
-
+    
     if result[0] == 'бронза':
         tech = 'литье'
     elif result[0] == 'глина':
         tech = 'лепка'
     elif result[0] == 'железо':
-        tech = 'ловка'
+        tech = 'ковка'
     elif result[0] == 'камень':
         tech = 'скалывание'
     elif result[0] == 'керамика':
         tech = 'лепка'
     elif result[0] == 'кость':
-        tech = 'лезьба'
+        tech = 'резьба'
     elif result[0] == 'медь':
         tech = 'литье'
+    
+    os.remove(temp_file)
+    
+    
+    material = result[0]
+    technique = tech
+    probability = f"{round(perc_pred * 100, 0)}%"
 
-    return {
-        "confidence": perc_pred,
-        "medium": result[0],
-        "tech": tech
-        }
+    return templates.TemplateResponse('index.html', {'request': request, 'material': material, 'technique': technique, 'probability': probability})
